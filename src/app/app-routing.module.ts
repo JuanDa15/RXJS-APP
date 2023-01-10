5631e1737214b2ad86ccdf3f64aee1f71da790e8
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-01-09 23:50:25 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ObservableComponent } from './pages/observable/observable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'observable',
+    component: ObservableComponent,
+    title: 'Observable'
+  },
+  {
+    path: '**',
+    redirectTo: 'observable'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
