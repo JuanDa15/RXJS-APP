13e2955754743c3458ac26e15ee4998ed9a2e3b0
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-01-11 00:01:42 -0500

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ObservableComponent } from './pages/observable/observable.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
 
 const routes: Routes = [
   {
     path: 'observable',
     component: ObservableComponent,
     title: 'Observable'
+  },
+  {
+    path: 'subscription',
+    component: SubscriptionComponent,
+    title: 'Subscription and unsubscription'
   },
   {
     path: '**',
