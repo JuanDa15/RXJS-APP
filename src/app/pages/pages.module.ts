ba9542e14a46ef58ca99ecb236e8cfd24270af63
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-01-11 00:02:16 -0500

    add new component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObservableComponent } from './observable/observable.component';
 import { ComponentsModule } from '../components/components.module';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 
 
 @NgModule({
   declarations: [
-    ObservableComponent
+    ObservableComponent,
+    SubscriptionComponent
   ],
   imports: [
     CommonModule,
