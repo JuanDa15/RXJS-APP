14f665cbed7e02952ca63ba4b54d0ddf7bbce3ea
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2023-01-26 17:48:07 -0500

    add map operator

--- src/app/pages/operators/map/map.component.ts
+++ src/app/pages/operators/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, map, Observable, range, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -8,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  public originalArr: number[];
+  public mappedArr: number[];
+  public src$: Observable<number>;
+  public sub: Subscription | null;
+
+  constructor() {
+    this.mappedArr = [];
+    this.originalArr = [];
+    this.sub = null;
+    this.src$ = range(1,10).pipe(
+      map<number, number>( val => {
+        this.originalArr.push(val);
+        return val * 10
+      })
+    )
+  }
 
   ngOnInit(): void {
+
+  }
+
+  public start() {
+    this.originalArr = [];
+    this.mappedArr = [];
+    this.sub = this.src$.subscribe({
+      next: (val) => {
+        this.mappedArr.push(val);
+      },
+      complete: () => {
+        this.sub?.unsubscribe();
+      }
+    });
   }
 
 }
